b0bde72b8944247f3150b0146739d3a1a2b8b6d1
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2022-05-10 21:33:10 +0530

    left part completed

--- web/src/components/topbar/topbar.jsx
+++ web/src/components/topbar/topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
 export default function topbar() {
   return (
@@ -7,7 +8,12 @@ export default function topbar() {
         <div className="left"> 
           <a href="#intro" className="logo">hari</a>
           <div className="itemContainer">
-            
+            <Person className="icons"/>
+            <span>11 22 33 44</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icons"/>
+            <span>harisanth123ps@gmail.com</span>
           </div>
         </div>
 
